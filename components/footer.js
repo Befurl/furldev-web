2e0036a1bacb3b690ba0ff68213679f3797c87e6
befurl <furl@furldev.com>
2021-09-30 14:07:35 +0100

    fixed footer design

--- components/footer.js
+++ components/footer.js
@@ -7,23 +7,25 @@ class Footer extends HTMLElement {
         this.innerHTML = `
         <footer>
             <div class="container-fluid footer">
-                <div class="row">
-                    <div class="col-lg-6 col-12">
-                        <p>Welcome</p>
-                        <p></p>
-                        <a href="">Projects</a>
-                        <p></p>
-                        <a href="">News</a>
-                        <p></p>
-                        <a href ="">About</a>
-                    </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <p>Welcome</p>
+                            <p></p>
+                            <a href="">Projects</a>
+                            <p></p>
+                            <a href="">News</a>
+                            <p></p>
+                            <a href ="">About</a>
+                        </div>
 
 
 
-                    <div class="col-lg-6 cofl-12">
-                        <p>Contact</p>
-                        <p></p>
-                        <p>Enquiries: matthew@furldev.com</p>
+                        <div class="col-lg-6 cofl-12">
+                            <p>Contact</p>
+                            <p></p>
+                            <p>Enquiries: matthew@furldev.com</p>
+                        </div>
                     </div>
                 </div>
             </div>
