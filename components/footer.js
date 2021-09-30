1bf2c60cd25a9cf948547c85ead2b7cb74667220
befurl <furl@furldev.com>
2021-09-30 13:46:05 +0100

    made the footer more suitable

--- components/footer.js
+++ components/footer.js
@@ -8,41 +8,22 @@ class Footer extends HTMLElement {
         <footer>
             <div class="container-fluid footer">
                 <div class="row">
-                    <div class="col-lg-4 col-12">
+                    <div class="col-lg-6 col-12">
                         <p>Welcome</p>
                         <p></p>
-                        <a href="">>Hello</a>
+                        <a href="">Projects</a>
                         <p></p>
-                        <a href="">>Goodbye</a>
+                        <a href="">News</a>
+                        <p></p>
+                        <a href ="">About</a>
                     </div>
 
-                    <div class="col-lg-4 col-12 footer-link-center">
-                        <div class="footer-link-center-before"></div>
-                        <p>More Information</p>
-                        <p></p>
-                        <a href="">>Terms and Conditions</a>
-                        <p></p>
-                        <a href="">>Contact us</a>
-                        <p></p>
-                        <a href="">>Delivery</a>
-                        <p></p>
-                        <a href="">>Returns</a>
-                        <p></p>
-                        <a href="">>Privacy Policy</a>
-                        <p></p>
-                        <a href="">>Cookie Policy</a>
 
-                        <div class="footer-link-center-after"></div>
-                    </div>
 
-                    <div class="col-lg-4 col-12">
-                        <p>Nowhere</p>
+                    <div class="col-lg-6 cofl-12">
+                        <p>Contact</p>
                         <p></p>
-                        <p><i class="fa fa-phone"></i> 00000 000000</p>
-                        <p></p>
-                        <p>General Enquiries: email@email.email</p>
-                        <p></p>
-                        <p>Sales Enquiries:   email1@email.email</p>
+                        <p>Enquiries: matthew@furldev.com</p>
                     </div>
                 </div>
             </div>
